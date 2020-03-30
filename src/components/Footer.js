63b1d6cf68799f1b034d27ea04ddeb40da1eec61
Chris Jones <christopheralexjones@gmail.com>
2020-03-30 13:59:11 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,6 @@ export default () => {
   return (
     <footer>
       <span>© {year} Loud KTown</span>
-      <span className='footer__right'>
-        <span className='code'>&lt;code/&gt;</span> by{' '}
-        <a className='link' href='https://www.nolanbuzanis.com' target='_blank'>
-          Nolan Buzanis
-        </a>{' '}
-        , with help from{' '}
-        <a className='link' href='http://alexjin.me' target='_blank'>
-          Alex Jin
-        </a>
-      </span>
     </footer>
   );
 };
